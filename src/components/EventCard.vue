32f726bde54706123437caa163f5d8a50604ea58
Joshua Bonner <joshua.bonner@outlook.com>
2023-06-30 10:40:24 -0400

    Setup

---
+++ src/components/EventCard.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I see dead people.',
+      derp: 'Im a derp.',
+    }
+  },
+}
+</script>
+<style></style>
