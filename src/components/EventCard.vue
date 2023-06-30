48c55ea26dc0ffa6331a9579cac1b97b4e9b3c04
Joshua Bonner <joshua.bonner@outlook.com>
2023-06-30 16:03:18 -0400

    Foothold

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,12 +1,52 @@
-<template></template>
+<template>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
+      <span>{{ event.attendess.length }} attending</span>
+    </div>
+  </router-link>
+</template>
 <script>
 export default {
   data() {
     return {
-      quote: 'I see dead people.',
-      derp: 'Im a derp.',
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19, 2018',
+        time: '10:00am',
+        attendess: [
+          { id: 'abc123', name: 'Adam Jahr' },
+          { id: 'def456', name: 'John Doe' },
+        ],
+      },
     }
   },
 }
 </script>
-<style></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
