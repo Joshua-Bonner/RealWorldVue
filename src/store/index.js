16f42034b490b86055e5439acd5ff541f99d0984
Joshua Bonner <joshua.bonner@outlook.com>
2023-07-03 15:54:02 -0400

    Event Create

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    events: [],
+  },
+  getters: {
+    getEventById: (state) => (id) => {
+      return state.events.find((event) => event.id === id)
+    },
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    },
+  },
   modules: {},
 })
