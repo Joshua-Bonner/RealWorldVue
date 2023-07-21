59b30c9cb02fc211ed246c536ea8917478e78cdc
Joshua Bonner <joshua.bonner@outlook.com>
2023-07-21 15:14:55 -0400

    Changed over some computed properties

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,28 +1,16 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <input
-      v-bind="$attrs"
-      :value="value"
-      @input="updateValue"
-      v-on="listeners"
-    />
+    <input v-bind="$attrs" :value="value" @input="updateValue" />
   </div>
 </template>
 <script>
 import { formFieldMixin } from '../mixins/formFieldMixin'
+
 export default {
   mixins: [formFieldMixin],
   props: {
     value: [String, Number],
-  },
-  computed: {
-    listeners() {
-      return {
-        ...this.$listeners,
-        input: this.updateValue,
-      }
-    },
   },
 }
 </script>
